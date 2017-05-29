2a33051bd7b2f7f64f3d3e0413ebeed244331f88
liuyanjun513 <yn739843@dal.ca>
2017-05-29 00:15:45 -0300

    Update Password.java

--- app/src/main/java/com/example/lenovo/cs3130_assignment2/Password.java
+++ app/src/main/java/com/example/lenovo/cs3130_assignment2/Password.java
@@ -6,38 +6,52 @@ package com.example.lenovo.cs3130_assignment2;
 
 
 public class Password {
-    private String password;
-
-    public Password(String password) {
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public int checkPassword(){
-        if(this.password.equals("password")){
+    public int checkPassword(String passwordText){
+        if(passwordText.equals("password")){
             return 1;
         }
-        else if(this.password.length()<=8){
+        else if(passwordText.length()<=8) {
             return 2;
         }
+        else if(passwordText.length()>25){
+            return 3;
+        }
         else{
+            boolean flag=false;
+            int count=0;
+            for(int i=0;i<passwordText.length();i++){
+                if(Character.isDigit(passwordText.charAt(i))){
+                    flag=true;
+                    count++;
+                }
+            }
+            if(flag==false)
+            {
+                return 4;
+            }
+            if(count==passwordText.length()){
+                return 5;
+            }
+
             return 0;
         }
     }
 
     public String rules(int number){
         if(number==1){
-            return "password can not be \"password\"";
+            return "Password can not be \"password\".";
         }
         else if(number==2){
-            return "password must be longer than 8 characters";
+            return "Password must be longer than 8 characters.";
+        }
+        else if(number==3){
+            return "Password can not be shorter than 25 characters.";
+        }
+        else if(number==4){
+            return "Password must include one digit at least.";
+        }
+        else if(number==5){
+            return "Password can be all digits.";
         }
         else{
             return "password is correct";
